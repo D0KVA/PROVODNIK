8874b85fca088ea548def7edc401cd6c534a65a6
D0KVA <isip_v.s.sokolov@mpt.ru>
2023-11-14 22:19:29 +0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provodnik
+{
+    internal class Menu
+    {
+        public static int Show(int minS, int maxS)
+        {
+            int pos = 3;
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("->");
+
+                key = Console.ReadKey();
+
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("  ");
+
+                if (key.Key == ConsoleKey.UpArrow && pos != minS)
+                    pos--;
+                else if (key.Key == ConsoleKey.DownArrow && pos != maxS)
+                    pos++;
+            } while (key.Key != ConsoleKey.Enter);
+            return pos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace provodnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Проводник");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            ShowPapka("C:\\");
+            void ShowPapka(string p)
+            {
+                Console.Clear();
+                string[] paths = Directory.GetDirectories(p);
+                string[] pathFiles = Directory.GetFiles(p);
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+                foreach ((string path, DriveInfo drive) in paths.Zip(allDrives, (p, d) => (p, d)))
+                {
+                    Console.Write("  " + allDrives);
+                    Console.Write($"  {drive.AvailableFreeSpace / (1024 * 1024 * 1024):N2} Гб свободно из {drive.TotalSize / (1024 * 1024 * 1024):N2} Гб");
+                }
+
+                int pos = Menu.Show(0, paths.Length - 1);
+                if (pos == -1)
+                    return;
+                ShowPapka(paths[pos]);
+            }
+        }
+    }
+}
